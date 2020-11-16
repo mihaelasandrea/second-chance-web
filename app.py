c91789d950a589808184cf784abf879fc4299170
mihaelasandrea <mihaelasandrea@yahoo.com>
2020-11-16 08:34:19 +0000

    Create post_ad template

--- app.py
+++ app.py
@@ -97,6 +97,12 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
+@app.route("/post_ad")
+def post_ad():
+    return render_template("post_ad.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
