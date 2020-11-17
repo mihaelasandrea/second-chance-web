d457d7e604286cd886da7dfcfeca98b223e57bc9
mihaelasandrea <mihaelasandrea@yahoo.com>
2020-11-17 20:25:29 +0000

    Add search functionality

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_ads():
     return render_template("ads.html", ads=ads)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    ads = list(mongo.db.ads.find({"$text": {"$search": query}}))
+    return render_template("ads.html", ads=ads)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
