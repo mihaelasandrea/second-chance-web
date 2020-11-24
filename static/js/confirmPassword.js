132400b5ce2c60aff0f8717ed2734cd2e0049cdb
mihaelasandrea <mihaelasandrea@yahoo.com>
2020-11-24 14:55:37 +0000

    Add delete modal to delete account button

--- static/js/confirmPassword.js
+++ static/js/confirmPassword.js
@@ -2,9 +2,9 @@ var check = function() {
   if (document.getElementById('password').value ==
     document.getElementById('confirm_password').value) {
     document.getElementById('message').style.color = 'green';
-    document.getElementById('message').innerHTML = 'matching';
+    document.getElementById('message').innerHTML = 'Matching';
   } else {
     document.getElementById('message').style.color = 'red';
-    document.getElementById('message').innerHTML = 'not matching';
+    document.getElementById('message').innerHTML = 'Please insert a matching password';
   }
 };
